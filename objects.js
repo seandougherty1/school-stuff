ad309d9aa63f154f5c49a024056965bce4bba8b6
seandougherty1 <15c16331@marling.school>
2020-09-22 14:23:36 +0100

    gang

---
+++ objects.js
@@ -0,0 +1,10 @@
+let recipie = {
+    title: "Mole", 
+    servings: 2, 
+    ingriedients: ["cinnamon","Cumin","cocoa"]
+}
+ console.log(recipie.title)
+ console.log("Serves: " + recipie.servings)
+ for (let i = 0; i < recipie.ingriedients.length; i++){
+     console.log(recipie.ingriedients[i])
+ }
