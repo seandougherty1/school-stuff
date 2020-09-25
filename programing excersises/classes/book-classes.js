f56cf33e3dd56180c2f483cdfeac1833cf9141fa
seandougherty1 <15c16331@marling.school>
2020-09-25 12:00:03 +0100

    Classes work

---
+++ programing excersises/classes/book-classes.js
@@ -0,0 +1,15 @@
+class Book{
+    constructor(){
+        this.book = book;
+        this.author =  author;
+        this.read = read;
+    }
+    library(){
+        console.log(`${this.book} by ${this.author} and it has been ${this.read}` )
+    }
+    
+}
+
+let book1 = new book('American Phsyco', 'Bret Easton Ellis', true)
+
+book1.library
---
+++ programing excersises/classes/starter.js
@@ -0,0 +1,9 @@
+class person{
+    constructor(name, age){
+        this.name = name
+        this.age
+    }
+    sayHello(){
+        console.log(`Hello ${this.name}`)
+    }
+}
